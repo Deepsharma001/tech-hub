6919c554bb0f68b8ed11653792565cfd5226f244
Deepak Sharma <44469858+Deepsharma001@users.noreply.github.com>
2020-01-19 17:18:43 +0530

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,14 +6,14 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-    <link rel="shortcut icon" type="image/x-icon" href="{{ URL::asset('public/admin_assets/img/favicon.ico') }}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('public/admin_assets/img/favicon.ico') }}">
     <title>Preclinic - Medical & Hospital - Bootstrap 4 Admin Template</title>
-    <link rel="stylesheet" type="text/css" href="{{ URL::asset('public/admin_assets/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ URL::asset('public/admin_assets/css/font-awesome.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ URL::asset('public/admin_assets/css/style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{asset('public/admin_assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{asset('public/admin_assets/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{asset('public/admin_assets/css/style.css') }}">
     <!--[if lt IE 9]>
-		<script src="{{ URL::asset('public/admin_assets/js/html5shiv.min.js') }}"></script>
-		<script src="{{ URL::asset('public/admin_assets/js/respond.min.js') }}"></script>
+		<script src="{{asset('public/admin_assets/js/html5shiv.min.js') }}"></script>
+		<script src="{{asset('public/admin_assets/js/respond.min.js') }}"></script>
 	<![endif]-->
 </head>
 
@@ -74,10 +74,10 @@
 			</div>
         </div>
     </div>
-    <script src="{{ URL::asset('public/admin_assets/js/jquery-3.2.1.min.js') }}"></script>
-	<script src="{{ URL::asset('public/admin_assets/js/popper.min.js') }}"></script>
-    <script src="{{ URL::asset('public/admin_assets/js/bootstrap.min.js') }}"></script>
-    <script src="{{ URL::asset('public/admin_assets/js/app.js') }}"></script>
+    <script src="{{asset('public/admin_assets/js/jquery-3.2.1.min.js') }}"></script>
+	<script src="{{asset('public/admin_assets/js/popper.min.js') }}"></script>
+    <script src="{{asset('public/admin_assets/js/bootstrap.min.js') }}"></script>
+    <script src="{{asset('public/admin_assets/js/app.js') }}"></script>
 </body>
 
 
