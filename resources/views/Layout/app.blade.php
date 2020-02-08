ddefc24b148020937013e20b75062d9135a9bdac
Deepak Sharma <44469858+Deepsharma001@users.noreply.github.com>
2020-02-08 09:50:31 +0530

    Update app.blade.php

--- resources/views/Layout/app.blade.php
+++ resources/views/Layout/app.blade.php
@@ -157,7 +157,7 @@
         <div class="row">
           <div class="col-md-12 text-center">
 
-            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            <p>
   Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved |<i class="fa fa-coffee" aria-hidden="true"></i> <a href="https://studybaba.in" target="_blank"><b>StudyBABA</b></a>
   <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
           </div>
